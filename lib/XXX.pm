a645a7192a7f09783f92d0550c8a818f5a9d1b5b
Ingy dot Net <ingy@ingy.net>
2010-07-17 14:45:12 -0700

    first commit

---
+++ lib/XXX.pm
@@ -0,0 +1,22 @@
+module XXX;
+
+use YAML;
+
+sub WWW($o) is export {
+    note YAML::dump($o);
+    return $o;
+}
+
+sub XXX($o) is export {
+    warn YAML::dump($o);
+    exit 1;
+}
+
+sub YYY($o) is export {
+    print YAML::dump($o);
+    return $o;
+}
+
+sub ZZZ($o) is export {
+    die YAML::dump($o);
+}
---
+++ t/ok.t
@@ -0,0 +1,5 @@
+use Test;
+
+use XXX;
+
+ok 'XXX modules loads without errors';
